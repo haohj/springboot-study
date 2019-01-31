b8a061ad78cbcbd674920a1177e7cbe2bc27038b
hhj <haohj94@163.com>
2019-01-31 11:05:50 +0800

    first commit

---
+++ chapter1/src/main/java/com/hao/chapter1/Chapter1Application.java
@@ -0,0 +1,14 @@
+package com.hao.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+}
+
