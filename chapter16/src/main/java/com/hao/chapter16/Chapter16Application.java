cac31a4a2900ebb4f3dd656c6d120c8a123a1aa1
郝红剑 <hongjian.hao@zjtzsw.com>
2020-06-03 11:22:57 +0800

    定时任务详解

--- chapter16/src/main/java/com/hao/chapter16/Chapter16Application.java
+++ chapter16/src/main/java/com/hao/chapter16/Chapter16Application.java
@@ -2,7 +2,14 @@ package com.hao.chapter16;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Chapter16Application {
 
@@ -10,4 +17,15 @@ public class Chapter16Application {
         SpringApplication.run(Chapter16Application.class, args);
     }
 
+    /**
+     * 很关键：默认情况下 TaskScheduler 的 poolSize = 1
+     *
+     * @return 线程池
+     */
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
+    }
 }
