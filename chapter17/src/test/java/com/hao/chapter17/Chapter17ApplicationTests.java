fea341af6ee327dc5a0df7a524129c0895230e85
郝红剑 <hongjian.hao@zjtzsw.com>
2020-06-04 09:19:59 +0800

    轻松搞定文件上传

---
+++ chapter17/src/test/java/com/hao/chapter17/Chapter17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.chapter17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
